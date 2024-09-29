3bf1f758d478c0e68b8d32679b645092d0ff159a
ValentinaDev <valenmdev@gmail.com>
2024-09-28 23:33:51 -0500

    version 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AccesoriosComponent } from './accesorios/accesorios.component';
+import { CobijasComponent } from './cobijas/cobijas.component';
+import { CojinesComponent } from './cojines/cojines.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { OtrosproductosComponent } from './otrosproductos/otrosproductos.component';
+import { TermosComponent } from './termos/termos.component';
+import { VivetufeComponent } from './vivetufe/vivetufe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainpageComponent }, // Ruta principal
+  { path: 'accesorios', component: AccesoriosComponent }, // Ruta para Accesorios
+  { path: 'cobijas', component: CobijasComponent }, // Ruta para Cobijas
+  { path: 'cojines', component: CojinesComponent }, // Ruta para Cojines
+  { path: 'nosotros', component: NosotrosComponent }, // Ruta para Nosotros
+  { path: 'otros-productos', component: OtrosproductosComponent }, // Ruta para Otros Productos
+  { path: 'termos', component: TermosComponent }, // Ruta para Termos
+  { path: 'vive-tu-fe', component: VivetufeComponent }, // Ruta para Vive tu Fe
+];
